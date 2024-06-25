24b43d6749d13caa341657e02d91e50789f8a14f
Kshitij Nishant <kshitijnishant09@gmail.com>
2024-06-25 13:18:43 +0530

    Data validation added

---
+++ src/textsumarrizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from textsumarrizer.config.configuration import ConfigurationManager
+from textsumarrizer.components.data_validation import DataValiadtion
+from textsumarrizer.logging import logger
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
